4431eeb494357daacb6b8d1ca2c7167fb114ad60
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-09-22 19:39:25 +0000

    Began writing function using conditional statements

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+* print_number - function will print numbers no matter their value
+* @n: represents value given to us in main function
+*/
+
+void print_number(int n)
+{
+
+	if (n > 9 && n <= 99)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar('\n');
+	}
+	else if (n > 99 && n <= 999)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar('\n');
+	}
+	else if (n > 999)
+	{
+		_putchar(n / 1000 + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar('\n');
+	}
+	else if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(n + '0');
+		_putchar('\n');
+	}
+}
