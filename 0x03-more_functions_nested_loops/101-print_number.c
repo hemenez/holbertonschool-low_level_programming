09adb95232780aef384b1cdab37c89056328bf8a
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-09-22 23:58:51 +0000

    Removed unnecessary spaces at the start of the line

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
 * print_number - function will print numbers no matter their value
 * @n: represents value given to us in main function
@@ -7,30 +6,30 @@
 
 void print_number(int n)
 {
-        int divisor;
-        int num;
-        int count;
+	int divisor;
+	int num;
+	int count;
 
-        num = n;
-        divisor = 1;
+	num = n;
+	divisor = 1;
 
-        if (n < 0)
-        {
-                n = -n;
-                num = -num;
-                _putchar('-');
-        }
-        for (count = 0; n / 10 > 0; count++)
-        {
-                n = n / 10;
-                divisor = divisor * 10;
-        }
-        while (count > 0)
-        {
-                _putchar(num / divisor + '0');
-                num = num % divisor;
-                divisor = divisor / 10;
-                --count;
-        }
-        _putchar(num + '0');
+	if (n < 0)
+	{
+		n = -n;
+		num = -num;
+		_putchar('-');
+	}
+	for (count = 0; n / 10 > 0; count++)
+	{
+		n = n / 10;
+		divisor = divisor * 10;
+	}
+	while (count > 0)
+	{
+		_putchar(num / divisor + '0');
+		num = num % divisor;
+		divisor = divisor / 10;
+		--count;
+	}
+	_putchar(num + '0');
 }
