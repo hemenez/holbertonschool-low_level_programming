cd575ac7a8e2bc752cb3f1d779c0b4d646cbdbb8
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-09-22 19:51:05 +0000

    Wrote a C program that prints size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program will print size of variable types after compiling
+ * Return: - The program will return a zero if compiled correctly
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
