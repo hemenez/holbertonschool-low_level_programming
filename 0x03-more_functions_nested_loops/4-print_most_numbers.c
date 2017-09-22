61069151de8ecf1633f381a35b2d419a26381047
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-09-22 19:34:26 +0000

    Wrote a function that prints numbers 0 through 9 excluding 2 and 4

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - function will print all numbers 0-9 except for 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+	_putchar('\n');
+}
