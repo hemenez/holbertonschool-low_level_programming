6ddffbf40a09cc07ea7082695196a504e1d752d0
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-09-22 21:36:36 +0000

    Added condition to change negative numbers to positive immediately

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,11 @@
 
 void print_number(int n)
 {
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
 	if (n > 9 && n <= 99)
 	{
 		_putchar(n / 10 + '0');
@@ -22,13 +27,6 @@ void print_number(int n)
 		_putchar(n / 1000 + '0');
 		_putchar((n / 100) % 10 + '0');
 		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n < 0)
-	{
-		n = -n;
-		_putchar('-');
-		_putchar(n / 10 + '0');
 		_putchar(n % 10 + '0');
 	}
 	else
