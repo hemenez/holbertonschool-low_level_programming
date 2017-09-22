af2c5b51d1d24b9c18cc854a26fb40caa78d2b64
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-09-22 19:33:46 +0000

    Wrote a function that prints numbers 0 through 9

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_numbers - function will print numbers 0 through 9
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar(num + '0');
+	_putchar('\n');
+}
