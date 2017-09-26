5680847cda18f8c96c9a6983d84411f096df1bec
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-09-26 17:42:33 +0000

    Added comments describing what function is doing

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- *
- *
+ * reset_to_98 - function will take a pointer and change value of an int
+ * @n: represents value project wants us to change to
  */
 
 void reset_to_98(int *n)
