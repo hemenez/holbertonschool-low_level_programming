23f3ab7cc54f80b7aa520128c51e64090afe668c
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-09-29 04:16:46 +0000

    Wrote a function to compare two strings

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strcmp - function will compare two strings
+ * @s1: represents pointer to first string
+ * @s2: represents pointer to second string
+ * Return: function will return a number based off comparison of strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+	int diff;
+
+	a = 0;
+	diff = s1[a] - s2[a];
+
+	if (s1[a] == s2[a])
+		return (diff);
+	else if (s1[a] < s2[a])
+		return (diff);
+	else
+		return (diff);
+}
