e3dd9e743adc72b129895ae767da1210395fa14b
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-09-29 01:40:38 +0000

    Wrote a function that copies a string

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *_strncpy - wrote a function that copies a string
+ * @dest: Represents destination variable
+ * @src: Represents variable that is having work done to it
+ * @n: Represents max byte size fed from main function
+ * Return: function will return destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	a = 0;
+
+	while (src[a] != '\0' && a < n)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	return (dest);
+}
