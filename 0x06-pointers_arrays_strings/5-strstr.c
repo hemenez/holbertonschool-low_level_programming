b543be0a446f1e02aa7100030a62f2c194794718
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-02 23:40:57 +0000

    Wrote a function to locate a substring

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * *_strstr - function will locate a substring
+ * @haystack: represents string to be searched
+ * @needle: represents substring
+ * Return: function will return located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+			return (haystack);
+		haystack++;
+	}
+	if (*needle == '\0')
+		return (needle);
+	return (NULL);
+}
