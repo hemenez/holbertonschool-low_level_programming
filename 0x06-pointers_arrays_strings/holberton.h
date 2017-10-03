7316f26257752240a7d428710e76eb1546573159
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-03 20:56:34 +0000

    Added function to header file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOBLERTON_H_
 #define HOLBERTON_H_
 
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
