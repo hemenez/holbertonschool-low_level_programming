b02680a3eddc3bb6d5ad01745e5d6a116ee9b99a
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-03 03:58:13 +0000

    Rewrote function to print substring

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -8,14 +8,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	unsigned int i;
+	unsigned int j;
 
-	while (*haystack != '\0')
+	i = 0;
+	while (haystack[i] != '\0')
 	{
-		if (*haystack == *needle)
-			return (haystack);
-		haystack++;
+		j = 0;
+		while (needle[j] != '\0')
+		{
+			if (haystack[i] == needle[j])
+				return (needle);
+			j++;
+		}
+		i++;
 	}
-	if (*needle == '\0')
+	if (needle[j] == '\0')
 		return (needle);
 	if (!*needle)
 		return (haystack);
