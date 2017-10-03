c545b966a4bdc915811ea8a839c84d382e9c1544
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-03 20:28:34 +0000

    Finalized test conditions

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -10,22 +10,22 @@ char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i;
 	unsigned int j;
+	unsigned int k;
 
 	i = 0;
 	while (haystack[i] != '\0')
 	{
-		j = 0;
-		while (needle[j] != '\0')
+		k = i;
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
-				return (needle);
-			j++;
+			if (haystack[k] == needle[j])
+				k++;
+			else
+				break;
 		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
 		i++;
 	}
-	if (needle[j] == '\0')
-		return (needle);
-	if (!*needle)
-		return (haystack);
 	return (NULL);
 }
