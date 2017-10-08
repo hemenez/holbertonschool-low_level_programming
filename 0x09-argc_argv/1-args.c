4ac58798e84e8c1b6f135d3eabdc288917fd9d14
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-08 22:28:26 +0000

    Added offset to account for counting first index

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -4,11 +4,15 @@
  * main - program will print number of arguments passed to it
  * @argc: represents argument count
  * @argv: represents string array, unused in this program
- * Return: 
+ * Return: function will return 0 if compiled properly
  */
 
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", argc);
+	int offset;
+
+	offset = argc - 1;
+
+	printf("%d\n", offset);
 	return (0);
 }
