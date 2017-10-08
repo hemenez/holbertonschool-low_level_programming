2f5613893fc3ed5dfaf58b6a0c8674272ba29e31
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-08 22:24:59 +0000

    Added descriptor for unused variable

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ * main - program will print number of arguments passed to it
+ * @argc: represents argument count
+ * @argv: represents string array, unused in this program
+ * Return: 
  */
 
 int main(int argc, char __attribute__((__unused__)) *argv[])
