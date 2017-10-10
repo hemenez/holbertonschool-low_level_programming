d6901ff0d05a9a2b36193222304b7bc80a58270d
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-10 20:31:13 +0000

    Added secondary conditional

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -17,7 +17,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(sizeof(char) * size);
-	for (i = 0; i < size; i++)
-		ptr[i] = c;
+	if (ptr != NULL)
+	{
+		for (i = 0; i < size; i++)
+			ptr[i] = c;
+	}
 	return (ptr);
 }
