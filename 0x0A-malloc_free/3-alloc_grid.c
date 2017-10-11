24ff76b9bad2b9d9a5a01ca8ec51c9aa7c092c90
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-11 02:35:42 +0000

    Input memory release

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -24,7 +24,12 @@ int **alloc_grid(int width, int height)
 		{
 			ptr[i] = (int *)malloc(width * sizeof(int));
 			if (ptr[i] == NULL)
+			{
+				for (; i >= 0; i--)
+					free(ptr[i]);
+				free(ptr);
 				return (NULL);
+			}
 			for (j = 0; j < width; j++)
 				ptr[i][j] = 0;
 		}
