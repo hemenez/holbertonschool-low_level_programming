ac464782d7679e0913446b825814f51393d5b2d1
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-11 02:29:04 +0000

    Adjusted allocation to handle double pointer

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -15,14 +15,14 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = malloc(height * sizeof(int));
+	ptr = (int **)malloc(height * sizeof(int *));
 	if (ptr == NULL)
 		return (NULL);
 	if (ptr != NULL)
 	{
 		for (i = 0; i < height; i++)
 		{
-			ptr[i] = malloc(width * sizeof(int));
+			ptr[i] = (int *)malloc(width * sizeof(int));
 			if (ptr[i] == NULL)
 				return (NULL);
 			for (j = 0; j < width; j++)
