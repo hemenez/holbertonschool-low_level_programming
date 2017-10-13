2cbeda2fa8453b97d4a0dace1b8e9cc1e9219985
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-13 03:39:57 +0000

    Finalized malloc call with loop

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
- *
- *
+ * array_range - function will return an array
+ * @min: represents minimum value
+ * @max: represents maximum value
+ * Return: function will return pointer to array
  */
 
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int i, j;
+	int i;
 
-	if (min > max)
+       if (min > max)
 		return (NULL);
-	ptr = malloc(min * max * sizeof(int));
+       ptr = malloc(sizeof(int) * max - min + 1);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i <= min; i++)
-		for (j = 0; j <= max; j++)
-			*ptr = 0;
+	for (i = 0; min <= max ; i++)
+		{
+			ptr[i] = min;
+			min++;
+		}
 	return (ptr);
 }
