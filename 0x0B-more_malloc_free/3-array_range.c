f2483ca85eae75af8b0616f29e122a424e162908
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-13 03:48:42 +0000

    Fixed betty style

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -13,15 +13,15 @@ int *array_range(int min, int max)
 	int *ptr;
 	int i;
 
-       if (min > max)
+	if (min > max)
 		return (NULL);
-       ptr = malloc(sizeof(int) * (max - min + 1));
+	ptr = malloc(sizeof(int) * (max - min + 1));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; min <= max ; i++)
-		{
-			ptr[i] = min;
-			min++;
-		}
+	for (i = 0; min <= max; i++)
+	{
+		ptr[i] = min;
+		min++;
+	}
 	return (ptr);
 }
