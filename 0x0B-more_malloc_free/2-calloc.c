b54ee26a4cce3ba0104ce1533ae19857fc8a953b
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-13 01:43:55 +0000

    Removed typecast

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = (void *) malloc(nmemb * size);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
