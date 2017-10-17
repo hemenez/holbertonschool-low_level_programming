206bc23a01aa1001a5a815a045dcf38738e26318
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-17 00:58:14 +0000

    Initialized header file holding function prototypes

---
+++ 0x0D-structures_typedef/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
