fbb9c8047215dd111ffb7eaabf833009fb9e0883
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-17 02:00:11 +0000

    Removed return statement

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -6,7 +6,6 @@
  * @name: represents name of dog
  * @age: represents age of dog
  * @owner: represents name of owner
- * Return: function will return void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -14,5 +13,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return;
 }
