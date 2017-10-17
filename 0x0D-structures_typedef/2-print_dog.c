8773ae624abfb750c7703bed0019d851210d6a39
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-17 02:38:23 +0000

    Removed float NULL test

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -11,8 +11,6 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	if (d->age == '0')
-		printf("Age: (nil)\n");
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	printf("Name: %s\n", d->name);
