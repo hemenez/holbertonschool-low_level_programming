561d5007b811d3f77eebd9d3ca1505646bfc3ec7
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-17 01:09:41 +0000

    Defined a new type using structures

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - new structure defined for Django
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
