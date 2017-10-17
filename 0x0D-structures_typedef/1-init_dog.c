d7169596d449c884414eabf92630279c5cd1b6bf
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-17 01:59:13 +0000

    Added return statement

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -6,6 +6,7 @@
  * @name: represents name of dog
  * @age: represents age of dog
  * @owner: represents name of owner
+ * Return: function will return void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -13,4 +14,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	return;
 }
