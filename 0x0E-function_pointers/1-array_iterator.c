0d03d42fdd68739c65e8c658c926616c8f14a3de
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-18 22:42:40 +0000

    Wrote function to iterate through array

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * array_iterator - function will iterate through array
+ * @array: represents array being passed
+ * @size: represents size of array
+ * @action: represents pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		(action)(array[i]);
+}
