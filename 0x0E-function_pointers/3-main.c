e59bdcc623850cdaa3705725373e3d2085662e20
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-19 06:35:02 +0000

    Changed formatting

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -24,17 +24,17 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	lil = argv[2];
-	if ((num2 == 0) && ((strcmp(lil, "%") == 0) || (strcmp(lil, "/") == 0)))
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	f = get_op_func(argv[2]);
 	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
+	}
+	lil = argv[2];
+	if ((num2 == 0) && ((strcmp(lil, "%") == 0) || (strcmp(lil, "/") == 0)))
+	{
+		printf("Error\n");
+		exit(100);
 	}
 	printf("%d\n", f(num1, num2));
 	return (0);
