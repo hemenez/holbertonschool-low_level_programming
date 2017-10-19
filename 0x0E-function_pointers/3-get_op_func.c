4ca7cb7b3586d50c0dd18a3db912e6046d9252e0
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-19 23:09:29 +0000

    Added comparison condition

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * get_op_func - function selects correct function to perform op
  * @s: represents operator passed from main
@@ -24,7 +25,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL && s != NULL)
 	{
-		if (*(ops[i].op) == *s)
+		if (strcmp((ops[i].op), s) == 0)
 			return (ops[i].f);
 		++i;
 	}
