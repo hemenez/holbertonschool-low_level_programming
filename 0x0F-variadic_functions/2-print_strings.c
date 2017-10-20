a12544655f8ad2e13802b31ed694ec1137ac21f1
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-20 21:22:15 +0000

    Wrote a function to print strings

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - function will print strings
+ * @separator: represents separator between strings
+ * @n: represents number of parameters
+ * Return: function returns void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *string;
+	unsigned int i;
+	va_list params;
+
+	if (separator == NULL)
+		return;
+	va_start(params, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(params, char *);
+		if (!string)
+			printf("(nil)");
+		printf("%s", string);
+		if (i < (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+}
