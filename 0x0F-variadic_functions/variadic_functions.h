6b9fe7d7a5af984cbb74d0d30523fae48fffe8df
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-21 20:25:19 +0000

    Fixed style checks against Betty

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -5,16 +5,16 @@
 typedef struct op
 {
 	char *given;
-	void (*f)();
+	void (*f)(void);
 } mover;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void string_print();
-void char_print();
-void int_print();
-void float_print();
+void string_print(void);
+void char_print(void);
+void int_print(void);
+void float_print(void);
 
 #endif
