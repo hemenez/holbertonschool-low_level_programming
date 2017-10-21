a2d90716285fbb9ca372a715b11f755697ede738
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-21 17:21:59 +0000

    Updated header file with struct

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,9 +1,20 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+
+typedef struct op
+{
+	char *given;
+	void (*f)();
+} mover;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void string_print();
+void char_print();
+void int_print();
+void float_print();
 
 #endif
