3a6e0e575c2a3229d70be737aa4e084ad016e70b
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-21 20:33:10 +0000

    Adjusted pointer NULL check

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(params, char *);
-		if (!string)
+		if (string == NULL)
 			printf("(nil)");
 		printf("%s", string);
 		if (i < (n - 1))
