505c315b21771fee64e5aa1dd354ce7510c8e560
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-31 19:13:30 +0000

    Accounted for NULL head node

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,6 +9,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *buff;
 
+	if (*head == NULL)
+		return;
 	while (*head)
 	{
 		buff = (*head);
@@ -16,4 +18,5 @@ void free_listint2(listint_t **head)
 		free(buff);
 	}
 	free(*head);
+	(*head) = NULL;
 }
