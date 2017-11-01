ba93e02545af747d8af0def2bf2f3fd166ee46e3
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-01 02:52:59 +0000

    Readded free statements

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,6 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		(*head) = buff->next;
+		free(buff);
 		return (1);
 	}
 	buff2 = buff->next;
@@ -25,13 +26,20 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	while (x < (index - 1) && buff != NULL)
 	{
 		if (buff == NULL)
+		{
+			free(buff);
 			return (-1);
+		}
 		if (buff2 == NULL)
+		{
+			free(buff);
 			return (-1);
+		}
 		buff = (*head);
 		(*head) = buff->next;
 		x++;
 	}
 	buff->next = buff2->next;
+	free(buff2);
 	return (1);
 }
