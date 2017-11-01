8e53c7fadc5434cb1dc8b551bc8fb6177d944248
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-01 01:03:26 +0000

    Removed NULL assignment

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (buff == NULL)
 		return (NULL);
 	buff->n = n;
-	buff->next = NULL;
 	if (idx == 0)
 	{
 		buff->next = (*head);
