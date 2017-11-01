8e98ea496fc70a79197e06a8cb14ccc473b06759
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-01 04:33:46 +0000

    Formatted NULL condition outside of loop

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,11 +22,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	buff2 = buff->next;
+	if (buff == NULL)
+		return (-1);
 	x = 0;
 	while (x < (index - 1) && buff != NULL)
 	{
-		if (buff == NULL)
-			return (-1);
 		buff = buff->next;
 		buff2 = buff2->next;
 		x++;
