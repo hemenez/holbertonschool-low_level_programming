ad6826eead633b3f03dc92589bbb9048c0a1b15a
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-01 03:31:26 +0000

    Added double condition check for NULL

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *buff;
 	listint_t *buff2;
 
-	if (head == NULL)
+	if ((*head) == NULL && head == NULL)
 		return (-1);
 	buff = *head;
 	if (index == 0)
@@ -21,22 +21,20 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(buff);
 		return (1);
 	}
-	buff2 = buff->next;
 	x = 0;
 	while (x < (index - 1) && buff != NULL)
 	{
 		if (buff == NULL)
 		{
-			free(buff);
 			return (-1);
 		}
 		if (buff2 == NULL)
 		{
-			free(buff);
 			return (-1);
 
 		}
 		buff = buff->next;
+		buff2 = buff->next;
 		x++;
 	}
 	buff->next = buff2->next;
