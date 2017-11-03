8a475328cba916c183d51e76317a3cc629178570
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-03 01:13:16 +0000

    Made comments more specific

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -9,8 +9,8 @@ void print_binary(unsigned long int n)
 {
 	if (n > 1) /** recursively iterate until last digit is 1 */
 		print_binary(n >> 1);
-	if (n & 1) /** if number going back up level compares to one, print 1 */
+	if (n & 1) /** if number going back up level and one match, print 1 */
 		_putchar('1');
-	else /** if number going back up level does not compare, print zero */
+	else /** if number going back up level does not match w/1, print zero */
 		_putchar ('0');
 }
