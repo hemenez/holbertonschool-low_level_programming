bda77457db327785980ddad0de127558a7cd67dc
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-08 22:59:23 +0000

    Added function prototypes for cp

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -9,5 +9,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void printerror(int x, char *argv);
+void closeerror(int x, int fd_val);
 
 #endif
