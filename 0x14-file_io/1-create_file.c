19972166fb543b4b62dd58f58c439a0bf35d782d
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-08 06:12:47 +0000

    Adjusted condition for text_content being empty

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,11 +15,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	x = open(filename, O_RDWR | O_CREAT, 0600);
 	if (x == -1)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
 	count = 0;
 	while (text_content[count] != '\0')
 		count++;
