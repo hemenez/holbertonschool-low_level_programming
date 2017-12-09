117be92a9fdf93f75043e8ee3ab3b9aea2d75fd3
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-09 00:09:13 +0000

    Wrote a function to add new node to dlinked list

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_dnodeint - function will add new node at beginning of list
+ * @head: represents pointer to pointer to head
+ * @n: reprents data to be implemented in new node
+ * Return: function will return address of new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+		(*head) = new;
+	}
+	else
+	{
+
+		new->next = (*head);
+		new->prev = NULL;
+		(*head) = new;
+	}
+	return (new);
+}
