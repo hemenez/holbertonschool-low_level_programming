e60958fba1d7ed8faff6777a8ab593fc642641aa
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-11 04:26:37 +0000

    Added NULL check

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(*new));
 	if (new == NULL)
 		return (NULL);
-	if (*h == NULL)
+	if (h == NULL || *h == NULL)
 		return (NULL);
 	tmp = *h;
 	new->n = n;
