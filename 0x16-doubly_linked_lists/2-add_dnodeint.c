eb315348347577ac93cd094d729c92b9a1a37fbf
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-11 03:54:59 +0000

    Added NULL check

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -10,10 +10,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	if (head == NULL)
+		return (NULL);
 	new = malloc(sizeof(*new));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	if (*head == NULL)
 	{
@@ -23,7 +24,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	else
 	{
-
 		new->next = (*head);
 		new->prev = NULL;
 		(*head) = new;
