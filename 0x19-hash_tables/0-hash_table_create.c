e19c2c176288ab277940e9d4f67dd71052e68c88
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-12 08:05:54 +0000

    Added setting of size member of the struct

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -12,10 +12,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	new_hash_table = malloc(sizeof(new_hash_table));
+	new_hash_table = malloc(sizeof(hash_table_t));
 	if (new_hash_table == NULL)
 		return (NULL);
-	new_hash_table->array = malloc(sizeof(new_hash_table->array) * size);
+	new_hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (new_hash_table->array == NULL)
 	{
 		free(new_hash_table);
@@ -27,5 +27,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		new_hash_table->array[i] = NULL;
 		i++;
 	}
+	new_hash_table->size = size;
 	return (new_hash_table);
 }
