ea86ad3b2d2d3ff3da1bef65a0e8353f2d703f24
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-12 11:39:41 +0000

    Removed iteration through whole array to incorporate hashtable purpose

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -11,40 +11,36 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	unsigned long int hash;
-	unsigned long int i;
 	hash_node_t *new_node;
+	hash_node_t *buff;
 
-	new_node = malloc(sizeof(new_node));
+	index = key_index(((unsigned char *)key), ht->size);
+	if (*key == '\0')
+		return (0);
+	if (key == NULL)
+		return (0);
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
-	hash = hash_djb2((unsigned char *)key);
-	if (ht->size < 1)
-		return (0);
-	index = hash % ht->size;
-	if (key[0] == '\0')
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
-	i = 0;
-	while (i < ht->size)
+	if (ht->array[index] == NULL)
 	{
-		if (i == index)
+		ht->array[index] = new_node;
+		return (1);
+	}
+	buff = ht->array[index];
+	while (buff != NULL)
+	{
+		if (strcmp(buff->key, key) == 0)
 		{
-			if (*ht->array == NULL)
-			{
-				new_node->next = NULL;
-				*ht->array = new_node;
-			}
-			else
-			{
-				new_node->next = *ht->array;
-				*ht->array = new_node;
-			}
+			buff->value = strdup(value);
 			return (1);
 		}
-		i++;
+		buff = buff->next;
 	}
-	return (0);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
