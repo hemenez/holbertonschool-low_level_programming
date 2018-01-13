98b0c715aa463820eebbaac604634c8b3857ba3f
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-13 04:46:57 +0000

    Adjusting malloc to allocate for pointer

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_hash_table = malloc(sizeof(hash_table_t));
 	if (new_hash_table == NULL)
 		return (NULL);
-	new_hash_table->array = malloc(sizeof(hash_node_t) * size);
+	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_hash_table->array == NULL)
 	{
 		free(new_hash_table);
