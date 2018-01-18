e226bf6f4c4b78e65389c392da11ad197b738780
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-18 21:27:23 +0000

    Added NULL check

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,6 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	hash_node_t *buff;
 
+	if (ht == NULL)
+		return (0);
 	index = key_index(((unsigned char *)key), ht->size);
 	if (*key == '\0')
 		return (0);
