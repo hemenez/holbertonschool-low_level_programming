e0c0aa44cc21e5a200ad99f19f8465f65b71b5b2
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-03-08 07:21:56 +0000

    Wrote a fxn to use post-order traversal in binary tree

---
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - fxn goes through tree using post-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a fxn to call for each node
+ * Return: function is void
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_postorder(tree->left, *func);
+	binary_tree_postorder(tree->right, *func);
+	(*func)(tree->n);
+}
