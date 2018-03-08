7fd824c9769163f719e698809547b839bb161269
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-03-08 07:22:22 +0000

    Wrote a fxn to measure depth of node in binary tree

---
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - fxn measures depth of a node in binary tree
+ * @node: pointer to the node to measure depth of
+ * Return: function returns depth of node, or 0 if node is NULL
+ */
+
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	size_t count;
+
+	if (node->parent == NULL || node == NULL)
+		return (0);
+	count = binary_tree_depth(node->parent);
+	return (count + 1);
+}
