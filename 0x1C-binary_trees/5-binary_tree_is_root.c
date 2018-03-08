cee8e5f6199c10b9d98c4e21dcfb218a77ef94a9
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-03-08 07:27:59 +0000

    Separated null check

--- 0x1C-binary_trees/5-binary_tree_is_root.c
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -7,7 +7,9 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent == NULL && node != NULL)
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
 		return (1);
 	return (0);
 }
